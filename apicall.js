2f7b0a508e319334efc64a34b4258cbd0be2f32d
George Smith <georgesmith4756@hotmail.co.uk>
2021-03-30 16:12:47 +0100

    initial codebase

--- apicall.js
+++ apicall.js
@@ -1,6 +1,11 @@
+<script type="text/javascript">
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
+
+</script>
